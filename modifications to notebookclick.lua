d52bf3d869f209fe35ffc6d9e9e192e765f8c4db
SparkIgnition <78872445+SparkIgnition1@users.noreply.github.com>
2024-03-26 16:42:57 -0500

    Update modifications to notebookclick.lua

--- modifications to notebookclick.lua
+++
@@ -1,9 +0,0 @@
--- Get the player's GUI
-local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
--- Get the UI
-local ui = playerGui:WaitForChild("YourUI") -- Change "YourUI" to the name of your UI
-
--- Make the UI visible when the notebook is clicked
-ui.Visible = true
